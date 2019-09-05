d709224fd6b6e2640a774dad144c3a725d4a5c1c
Jonathan Morales <johnathanmorales@Administrators-MacBook-Pro-67.local>
2019-09-05 16:40:38 -0400

    v.1.1

--- src/components/BundleCard/Details/index.js
+++ src/components/BundleCard/Details/index.js
@@ -4,48 +4,48 @@ import List from "./List";
 class Details extends React.Component {
   state = {
     showing: "Internet"
-  }
+  };
 
-  handleChangeList = (service) => {
-    console.log(service)
-  }
+  handleChangeList = service => {
+    this.setState({
+      showing: service
+    });
+  };
 
   render() {
     let ListShowing = <div></div>;
-    if (this.state.showing === 'Internet') {
+    if (this.state.showing === "Internet") {
       ListShowing = this.props.internet.map(Service => {
-        return <List service={Service} />;
-      })
-    } else if (this.state.showing === 'Tv') {
+        return <List service={Service} key={Service} />;
+      });
+    } else if (this.state.showing === "Tv") {
       ListShowing = this.props.tv.map(Service => {
-        return <List service={Service} />;
-      })
-    } else if (this.state.showing === 'Voice') {
+        return <List service={Service} key={Service} />;
+      });
+    } else if (this.state.showing === "Voice") {
       ListShowing = this.props.tv.map(Service => {
-        return <List service={Service} />;
-      })
+        return <List service={Service} key={Service} />;
+      });
     }
-
 
     return (
       <div>
         <div>
-          {
-            this.props.internet &&
-            <button onClick={() => this.handleChangeList('Internet')}>Internet</button>
-          }
-          {
-            this.props.tv &&
-            <button onClick={() => this.handleChangeList('Tv')}>Tv</button>
-          }
-          {
-            this.props.voice && 
-            <button onClick={() => this.handleChangeList('Voice')}>Voice</button>
-          }
+          {this.props.internet && (
+            <button onClick={() => this.handleChangeList("Internet")}>
+              Internet
+            </button>
+          )}
+          {this.props.tv && (
+            <button onClick={() => this.handleChangeList("Tv")}>Tv</button>
+          )}
+          {this.props.voice && (
+            <button onClick={() => this.handleChangeList("Voice")}>
+              Voice
+            </button>
+          )}
         </div>
-        {
-          ListShowing
-        }
+        {ListShowing}
       </div>
     );
   }
